a48c01f0dc4413f790b872e0b6a25fdf59578330
Rahulshaju <rahulshaju135@gmail.com>
2023-10-06 17:25:07 +0530

    upadates in rsb

--- components/shared/sidebar/RightSidebar.tsx
+++ components/shared/sidebar/RightSidebar.tsx
@@ -1,138 +1,93 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import RenderTag from "./RenderTag";
 
+const hotQuestions = [
+  { _id: 1, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 2, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 3, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 4, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 5, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 6, title: "How do I use express as a custom server in NextJS?" },
+];
+const popularTags = [
+  {
+    _id: 1,
+    name: "NextJS",
+    totalQuestions: 6,
+  },
+  {
+    _id: 2,
+    name: "Redux ToolKit",
+    totalQuestions: 6,
+  },
+  {
+    _id: 3,
+    name: "React",
+    totalQuestions: 3,
+  },
+  {
+    _id: 4,
+    name: "NextJS",
+    totalQuestions: 6,
+  },
+  {
+    _id: 5,
+    name: "NextJS",
+    totalQuestions: 6,
+  },
+];
 const RightSidebar = () => {
   return (
     <section className=" custom-scrollbar background-light900_dark200 light-border sticky right-0  top-0 flex h-screen w-[350px] flex-col  gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          <Link
-            href="/"
-            className="flex cursor-pointer items-center justify-between gap-7"
-          >
-            <p className="body-medium text-dark500_light700">
-              {" "}
-              can i get the course for free ?
-            </p>
-            <Image
-              src="/assets/icons/chevron-right.svg"
-              alt="Chevron-right-icon"
-              width={20}
-              height={20}
-              className="invert-colors"
-            />
-          </Link>
-          <Link
-            href="/"
-            className="flex cursor-pointer items-center justify-between gap-7"
-          >
-            <p className="body-medium text-dark500_light700">
-              {" "}
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s
-            </p>
-            <Image
-              src="/assets/icons/chevron-right.svg"
-              alt="Chevron-right-icon"
-              width={20}
-              height={20}
-              className="invert-colors"
-            />
-          </Link>
-          <Link
-            href="/"
-            className="flex cursor-pointer items-center justify-between gap-7"
-          >
-            <p className="body-medium text-dark500_light700">
-              {" "}
-              can i get the course for free ?
-            </p>
-            <Image
-              src="/assets/icons/chevron-right.svg"
-              alt="Chevron-right-icon"
-              width={20}
-              height={20}
-              className="invert-colors"
-            />
-          </Link>
-          <Link
-            href="/"
-            className="flex cursor-pointer items-center justify-between gap-7"
-          >
-            <p className="body-medium text-dark500_light700">
-              {" "}
-              can i get the course for free ?
-            </p>
-            <Image
-              src="/assets/icons/chevron-right.svg"
-              alt="Chevron-right-icon"
-              width={20}
-              height={20}
-              className="invert-colors"
-            />
-          </Link>
-          <Link
-            href="/"
-            className="flex cursor-pointer items-center justify-between gap-7"
-          >
-            <p className="body-medium text-dark500_light700">
-              {" "}
-              can i get the course for free ?
-            </p>
-            <Image
-              src="/assets/icons/chevron-right.svg"
-              alt="Chevron-right-icon"
-              width={20}
-              height={20}
-              className="invert-colors"
-            />
-          </Link>
+          {hotQuestions.map((question, index) => {
+            return (
+              <Link
+                key={question._id}
+                href={`/questions/${question._id}`}
+                className="flex cursor-pointer items-center justify-between gap-7"
+              >
+                <p className="body-medium text-dark500_light700">
+                  {" "}
+                  {question.title}
+                </p>
+                <Image
+                  src="/assets/icons/chevron-right.svg"
+                  alt="Chevron-right-icon"
+                  width={20}
+                  height={20}
+                  className="invert-colors"
+                />
+              </Link>
+            );
+          })}
         </div>
         <div></div>
       </div>
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          <Link href="/" className="flex justify-between gap-2">
+          {popularTags.map((tag) => {
+            return (
+              <RenderTag
+                key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                totalQuestions={tag.totalQuestions}
+                showCount
+              />
+            );
+          })}
+          {/* <Link href="/" className="flex justify-between gap-2">
             <Button className="background-light800_dark300 text-light400_light500">
               nextjs
             </Button>
             <p className="small-medium text-dark500_light700 text-white">6</p>
-          </Link>
-          <Link href="/" className="flex justify-between gap-2">
-            <Button className="background-light800_dark300 text-light400_light500">
-              nextjs
-            </Button>
-            <p className="small-medium text-dark500_light700 text-white">6</p>
-          </Link>
-          <Link href="/" className="flex justify-between gap-2">
-            <Button className="background-light800_dark300 text-light400_light500">
-              nextjs
-            </Button>
-            <p className="small-medium text-dark500_light700 text-white">6</p>
-          </Link>
-          <Link href="/" className="flex justify-between gap-2">
-            <Button className="background-light800_dark300 text-light400_light500">
-              nextjs
-            </Button>
-            <p className="small-medium text-dark500_light700 text-white">6</p>
-          </Link>
-          <Link href="/" className="flex justify-between gap-2">
-            <Button className="background-light800_dark300 text-light400_light500">
-              nextjs
-            </Button>
-            <p className="small-medium text-dark500_light700 text-white">6</p>
-          </Link>
-          <Link href="/" className="flex justify-between gap-2">
-            <Button className="background-light800_dark300 text-light400_light500">
-              nextjsadfafa
-            </Button>
-            <p className="small-medium text-dark500_light700 text-white">6</p>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </section>
